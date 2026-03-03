c9c43f786ed1deebc14ce619a6364ff568c111b5
zxraven <zx.raven@gmail.com>
2026-03-04 00:14:03 +0200

    Create tests/epam-client-work.spec.ts

---
+++ tests/epam-client-work.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+
+test('EPAM: Services -> Explore Our Client Work -> Client Work visible', async ({ page }) => {
+  // TODO: steps will be added in subsequent edits
+});
